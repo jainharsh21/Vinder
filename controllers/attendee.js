e81bd46f9b5907e8dc0ed18e59c28384e5e01af6
HARSH JAIN <jainharsh270@gmail.com>
2020-09-08 20:19:02 +0530

    get all attendees from a particular event

--- controllers/attendee.js
+++ controllers/attendee.js
@@ -1,3 +1,20 @@
+module.exports.getAllAttendeesFromEvent = async (req, res) => {
+  try {
+    const { db } = req.app.locals;
+    const data = await db.query(
+      `SELECT s.name,s.imageurl FROM attendees a JOIN users s ON a.stu_id=s.id WHERE a.event_id = ${req.params.event_id}`
+    );
+    console.log(data);
+    res.send(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message:
+        "We are expreincing issues please try again or contact the technical team",
+    });
+  }
+};
+
 module.exports.addAttendee = async (req, res) => {
   try {
     const { db } = req.app.locals;
--- route.js
+++ route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const { getAllEvents,addEvent } = require("./controllers/event");
 const { getAllUsers, addUser } = require("./controllers/user");
-const {addAttendee} = require("./controllers/attendee");
+const {addAttendee,getAllAttendeesFromEvent} = require("./controllers/attendee");
 const {
   getAllStudentChapters,
   addStudentChapter,
@@ -12,6 +12,7 @@ router.get("/events", getAllEvents);
 router.post('/events',addEvent);
 router.get("/users", getAllUsers);
 router.get("/student_chapters", getAllStudentChapters);
+router.get("/attendees/:event_id", getAllAttendeesFromEvent);
 router.post("/users", addUser);
 router.post("/student_chapters", addStudentChapter);
 router.post("/attendees", addAttendee);

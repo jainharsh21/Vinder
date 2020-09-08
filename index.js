d3518511d703ed5d9faefe506a92fe3b6774d59b
rampa2510 <iamram2510@gmail.com>
2020-09-08 15:25:50 +0530

    Initialsied directory

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 require('dotenv').config()
-const {Pool,Client} = require('pg')
+const {Pool} = require('pg')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(require('cors')());
 
 
-const client = new Client({
+
+const client = new Pool({
     user: 'postgres',
     host: '2.tcp.ngrok.io',
-    database: 'postgres',
+    database: 'vinder',
     password:process.env.DB_PASS,
     port: '11655',
     ssl: false
 })
 
 
-client.connect().then(d=>{
-    console.log("lol")
-}).catch(e=>{
-    console.log('lmao')
+app.listen(process.env.PORT,async()=>{
+    console.log(`Server Started on port ${process.env.PORT}`)
+    const data = await client.query("SELECT * FROM events")
+    console.log(data)
 })
+

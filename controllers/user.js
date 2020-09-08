36f325de6eb3977baf2498e07673f49458e4fc7a
HARSH JAIN <jainharsh270@gmail.com>
2020-09-08 19:07:38 +0530

    added add user functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const { hash, compare } = require("bcryptjs");
+
 module.exports.getAllUsers = async (req, res) => {
   try {
     const { db } = req.app.locals;
@@ -6,11 +8,27 @@ module.exports.getAllUsers = async (req, res) => {
     res.send(data.rows);
   } catch (err) {
     console.log(err);
-    res
-      .status(500)
-      .send({
-        message:
-          "We are expreincing issues please try again or contact the technical team",
-      });
+    res.status(500).send({
+      message:
+        "We are expreincing issues please try again or contact the technical team",
+    });
+  }
+};
+
+module.exports.addUser = async (req, res) => {
+  try {
+    const { db } = req.app.locals;
+    const hashPass =await hash(req.body.password, 2);
+    const data = await db.query(`INSERT INTO public.users(
+         name, email, password, bio, sex, imageurl)
+        VALUES ( '${req.body.name}','${req.body.email}' ,'${hashPass}' ,'${req.body.bio}' ,'${req.body.sex}' ,'${req.body.imageUrl}' );`);
+    console.log(data);
+    res.send(req.body);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message:
+        "We are expreincing issues please try again or contact the technical team",
+    });
   }
 };
--- route.js
+++ route.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const { getAllEvents } = require("./controllers/event");
 const { getAllUsers } = require("./controllers/user");
+const { addUser } = require("./controllers/user");
 
 router.get("/events", getAllEvents);
 router.get("/users", getAllUsers);
+router.post("/users",addUser)
 
 module.exports = router;

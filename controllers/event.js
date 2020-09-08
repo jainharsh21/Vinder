db0d38bc05bfc3ec272ca19ba3a530556437d013
rampa2510 <iamram2510@gmail.com>
2020-09-08 19:49:50 +0530

    Added route for adding new event

--- controllers/event.js
+++ controllers/event.js
@@ -10,3 +10,16 @@ module.exports.getAllEvents = async (req,res)=>{
     }
 }
 
+module.exports.addEvent = async (req,res)=>{
+    try{
+        const {db} = req.app.locals
+        const data = await db.query(`INSERT INTO public.events(
+            chp_id, name, summary, description, date, imageurl)
+            VALUES (${req.body.chpid},'${req.body.name}', '${req.body.summary}', '${req.body.description}', '${req.body.time}', '${req.body.imgUrl}')`)
+            console.log(data)
+            res.send("lol")
+    }catch(err){
+        console.log(err)
+        res.status(500).send({message:'We are expreincing issues please try again or contact the technical team'})
+    }
+}
--- route.js
+++ route.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
-const { getAllEvents } = require("./controllers/event");
+const { getAllEvents,addEvent } = require("./controllers/event");
 const { getAllUsers } = require("./controllers/user");
 
 router.get("/events", getAllEvents);
+router.post('/events',addEvent);
 router.get("/users", getAllUsers);
 
 module.exports = router;

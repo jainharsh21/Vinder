00f72a65d31b9079b47b15fa95397255eff3c28c
rampa2510 <iamram2510@gmail.com>
2020-09-09 19:37:46 +0530

    Added code to jwt verify

--- controllers/attendee.js
+++ controllers/attendee.js
@@ -1,11 +1,22 @@
+const {verifyToken} = require('../jwtHelper')
+
+
 module.exports.getAllAttendeesFromEvent = async (req, res) => {
   try {
     const { db } = req.app.locals;
-    const data = await db.query(
-      `SELECT s.name,s.imageurl FROM attendees a JOIN users s ON a.stu_id=s.id WHERE a.event_id = ${req.params.event_id}`
-    );
-    console.log(data);
-    res.send(data.rows);
+    const token = req.headers.authorization.split(" ")[1];
+    // console.log(token)
+    const isUserVerified = verifyToken(token)
+    // console.log(isUserVerified)
+    if(isUserVerified){
+      const data = await db.query(
+        `SELECT s.name,s.imageurl FROM attendees a JOIN users s ON a.stu_id=s.id WHERE a.event_id = ${req.params.event_id}`
+      );
+      // console.log(data);
+      res.send(data.rows);
+      return
+    }
+    res.status(400).send({message:"Unauthorized"})
   } catch (err) {
     console.log(err);
     res.status(500).send({

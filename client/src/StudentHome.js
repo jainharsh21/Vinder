ab96d9ffed9f866b3f88999122edcc8dfc613f2a
Ram Pandey <rampa2510@Rams-MacBook-Air.local>
2020-11-07 13:03:06 +0530

    Added animation

--- client/src/StudentHome.js
+++ client/src/StudentHome.js
@@ -4,8 +4,33 @@ import Navbar from "./Navbar";
 import "./Navbar.css";
 import { ChevronLeft, ChevronRight } from "@material-ui/icons";
 import EventCard from "./EventCard";
+import { useTransition, animated } from "react-spring";
 
 function StudentHome() {
+  const pages = [
+    ({ style }) => (
+      <animated.div style={{ ...style, background: "lightpink" }}>
+        <EventCard />
+      </animated.div>
+    ),
+    ({ style }) => (
+      <animated.div style={{ ...style, background: "lightblue" }}>
+        <EventCard />
+      </animated.div>
+    ),
+    ({ style }) => (
+      <animated.div style={{ ...style, background: "lightgreen" }}>
+        <EventCard />
+      </animated.div>
+    ),
+  ];
+  const [index, set] = React.useState(0);
+  const onClick = React.useCallback(() => set((state) => (state + 1) % 3), []);
+  const transitions = useTransition(index, (p) => p, {
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+  });
   return (
     <div>
       <Navbar />
@@ -25,9 +50,13 @@ function StudentHome() {
         >
           <ChevronLeft />
         </IconButton>
-        <EventCard />
+        {transitions.map(({ item, props, key }) => {
+          const Page = pages[item];
+          return <Page key={key} style={props} />;
+        })}
         <IconButton
           rounded
+          onClick={onClick}
           text={false}
           color="black"
           style={{ padding: "8px" }}

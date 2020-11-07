41fa54ebaec0cc3c2c6da97f8afb5ce18987ebfb
Ram Pandey <rampa2510@Rams-MacBook-Air.local>
2020-11-07 13:06:33 +0530

    fixed animation

--- client/src/StudentHome.js
+++ client/src/StudentHome.js
@@ -28,7 +28,7 @@ function StudentHome() {
   const onClick = React.useCallback(() => set((state) => (state + 1) % 3), []);
   const transitions = useTransition(index, (p) => p, {
     from: { opacity: 0, transform: "translate3d(100%,0,0)" },
-    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(-4%,0,0)" },
     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
   });
   return (
